85b4f9c324f77f6b9f2d4ee4ac2d78ebdc7fc32e
Darren Chau <darren.chau@cooper.edu>
2024-04-16 18:20:29 -0400

    Update Setup.sql

--- Database/Setup.sql
+++ Database/Setup.sql
@@ -3,3 +3,9 @@ insert into Users values (2,'dataman','password','organizer@cooper.edu',1);
 insert into Users values (3,'eventgoer','swordfish','personal@gmail.com',0);
 insert into Users values (4,'andy','password123','helpme@yahoo.com',0);
 insert into Users values (5,'rusty','xyzabc','throwaway@cooper.edu',0);
+
+insert into Events values();
+insert into Events values();
+insert into Events values();
+insert into Events values();
+insert into Events values();
