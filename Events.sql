7bb28637d8649192c27882a08fdba0e8e76b457f
Darren Chau <darren.chau@cooper.edu>
2024-04-16 17:58:45 -0400

    Update Events.sql

--- Events.sql
+++ Events.sql
@@ -35,3 +35,9 @@ insert into Users values (2,'dataman','password','organizer@cooper.edu',1);
 insert into Users values (3,'eventgoer','swordfish','personal@gmail.com',0);
 insert into Users values (4,'andy','password123','helpme@yahoo.com',0);
 insert into Users values (5,'rusty','xyzabc','throwaway@cooper.edu',0);
+
+insert into Events values(1, 'Ice Cream Social', 'IEEE', 'Food', 'Room 3', 50, '12/09/2024');
+insert into Events values(2, 'Talent Show', 'Elementary School', 'Performance', 'Auditoriom', 100, '01/04/2024');
+insert into Events values(3, 'Synergizing the Agile Method', 'Boeing', 'Conference', 'Room 2', 30, '07/24/2023');
+insert into Events values(4, 'Woodworking', 'Community', 'Workshop', 'Room 56', 15, '04/19/2024');
+
