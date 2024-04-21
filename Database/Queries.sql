9836f707b1c6ef0b1372ce6b38b1ca95789a4fd9
Darren Chau <darren.chau@cooper.edu>
2024-04-21 12:02:32 -0400

    Update Queries.sql

--- Database/Queries.sql
+++ Database/Queries.sql
@@ -1,1 +1,26 @@
+/*
+Some sample queries we might implement
+*/
+
+--Want to have five event types: Food, Workshop, Conference, Performance, Speaker
+--Queries to get events of each type
+SELECT Events.ename, Events.organizer, Events.location, Events.capacity, Events.time, Events.date
+FROM Events
+WHERE Events.type = 'Food'
+  
+SELECT Events.ename, Events.organizer, Events.location, Events.capacity, Events.time, Events.date
+FROM Events
+WHERE Events.type = 'Workshop'
+  
+SELECT Events.ename, Events.organizer, Events.location, Events.capacity, Events.time, Events.date
+FROM Events
+WHERE Events.type = 'Conference'
+  
+SELECT Events.ename, Events.organizer, Events.location, Events.capacity, Events.time, Events.date
+FROM Events
+WHERE Events.type = Performance' 
+  
+SELECT Events.ename, Events.organizer, Events.location, Events.capacity, Events.time, Events.date
+FROM Events
+WHERE Events.type = Speaker' 
 
