d24b5ff153b5ee3e738cda7d61af9af34ee07f87
Darren Chau <darren.chau@cooper.edu>
2024-04-21 12:53:22 -0400

    Update models.py

--- sql_app/models.py
+++ sql_app/models.py
@@ -3,3 +3,11 @@ from sqlalchemy.orm import relationship
 
 from .database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+class Events(Base):
+    __tablename__ = "users"
+
+class Reservations(Base):
+    __tablename__ = "users"
