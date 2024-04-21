5fa16aabb4182f5d99d9f2dfa97b3e9b03d277b8
Darren Chau <darren.chau@cooper.edu>
2024-04-21 11:28:11 -0400

    Update Setup.sql

--- Database/Setup.sql
+++ Database/Setup.sql
@@ -4,8 +4,8 @@ insert into Users values (3,'eventgoer','swordfish','personal@gmail.com',0);
 insert into Users values (4,'andy','password123','helpme@yahoo.com',0);
 insert into Users values (5,'rusty','xyzabc','throwaway@cooper.edu',0);
 
-insert into Events values(1, 'Ice Cream Social', 'IEEE', 'Food', 'Room 506', 35, '14:30', '03/06/2024');
-insert into Events values(2, 'Basket Weaving', 'BWE', 'Workshop', 'Room 206', 20, '14:30', '03/06/2024');
-insert into Events values(3, 'The Agile Method', 'BIE', 'Conference', 'Room LL201', 40, '08:30', '02/26/2024');
-insert into Events values(4, 'Talent Show', 'Peter Cooper', 'Performance', 'Rose Auditorium', 80, '12:30', '01/15/2024');
-insert into Events values(5, 'Famous Guest', 'TBA', 'Speaker', 'Room 101', 50, '20:15', '04/10/2024');
+insert into Events values(1, 'Ice Cream Social', 'IEEE', 'Food', 'Room 506', 35, 0, '14:30', '03/06/2024');
+insert into Events values(2, 'Basket Weaving', 'BWE', 'Workshop', 'Room 206', 20, 0, '14:30', '03/06/2024');
+insert into Events values(3, 'The Agile Method', 'BIE', 'Conference', 'Room LL201', 40, 5, '08:30', '02/26/2024');
+insert into Events values(4, 'Talent Show', 'Peter Cooper', 'Performance', 'Rose Auditorium', 80, 0, '12:30', '01/15/2024');
+insert into Events values(5, 'Famous Guest', 'TBA', 'Speaker', 'Room 101', 50, 8, '20:15', '04/10/2024');
