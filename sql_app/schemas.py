470634e6df51974e547f23c1da6a41fd665bab64
Darren Chau <darren.chau@cooper.edu>
2024-04-21 14:59:41 -0400

    Update schemas.py

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -1,1 +1,15 @@
 from pydantic import BaseModel
+
+class ReservationBase(BaseModel):
+    title: str
+    description: str | None = None
+
+class ReservationCreate(ReservationBase):
+    pass
+
+class Reservations(ReservationBase):
+    uid: int
+    eid: int
+
+    class Config:
+        orm_mode = True
