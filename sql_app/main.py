812c3b74b8cf14aab18c9be5f638318a73f16725
malek1038 <malek10381038@gmail.com>
2024-04-23 19:41:18 -0400

    Created a bunch of APIs
    Created all the APIs that I could think of, we can add more as we go and find out we need something. Note that there are not delete apis because Im actually not sure how that works, but I'll look into it. Also creating a reservation correctly updates the number of reservations for that Event, but that was the last thing I added, so the numbers might be wrong for some events I was testing before adding it (we'll prob just clean up the whole database eventually anyways.)

--- sql_app/main.py
+++ sql_app/main.py
@@ -56,3 +56,105 @@ def create_user_route(user: UserCreate, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not created")
     return "User " + db_user.uname + " created successfully"
+
+class EventCreate(BaseModel):
+    ename: str
+    organizer: str
+    type: str
+    location: str
+    capacity: int
+    reservations: int
+    time: str
+    date: str
+
+def create_event(db: Session, event: EventCreate):
+    db_event = Events(ename=event.ename, organizer=event.organizer, type=event.type, location=event.location, capacity=event.capacity, reservations=event.reservations, time=event.time, date=event.date)
+    db.add(db_event)
+    db.commit()
+    db.refresh(db_event)
+    return db_event
+
+@app.post("/createEvent/")
+def create_event_route(event: EventCreate, db: Session = Depends(get_db)):
+    db_event = create_event(db, event)
+    if db_event is None:
+        raise HTTPException(status_code=404, detail="Event not created")
+    return "Event " + db_event.ename + " created successfully"
+
+def get_event_by_id(db: Session, event_id: int):
+    event = db.query(Events).filter(Events.eid == event_id).first()
+    if event is None:
+        raise HTTPException(status_code=404, detail="Event not found")
+    return event
+
+@app.get("/events/{event_id}")
+def read_event(event_id: str, db: Session = Depends(get_db)):
+    event = get_event_by_id(db, int(event_id))
+    return event
+
+def get_event_by_name(db: Session, event_name: str):
+    event = db.query(Events).filter(Events.ename == event_name).first()
+    if event is None:
+        raise HTTPException(status_code=404, detail="Event not found")
+    return event
+
+@app.get("/eventByName/{event_name}")
+def read_event(event_name: str, db: Session = Depends(get_db)):
+    event = get_event_by_name(db, event_name)
+    return event
+
+class ReservationCreate(BaseModel):
+    uid: int
+    eid: int
+
+def create_reservation(db: Session, reservation: ReservationCreate):
+    db_reservation = Reservations(uid=reservation.uid, eid=reservation.eid)
+    db.add(db_reservation)
+    db.commit()
+    db.refresh(db_reservation)
+    return db_reservation
+
+def UpdateEventReservations(db: Session, event_id: int, num_reservations: int):
+    event = db.query(Events).filter(Events.eid == event_id).first()
+    if event is None:
+        raise HTTPException(status_code=404, detail="Event not found")
+    event.reservations = num_reservations
+    db.commit()
+    db.refresh(event)
+    return event
+    
+@app.post("/createReservation/")
+def create_reservation_route(reservation: ReservationCreate, db: Session = Depends(get_db)):
+    if get_user_by_id(db, reservation.uid) is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    if get_event_by_id(db, reservation.eid) is None:
+        raise HTTPException(status_code=404, detail="Event not found")
+    db_reservation = create_reservation(db, reservation)
+    if db_reservation is None:
+        raise HTTPException(status_code=404, detail="Reservation not created")
+    
+    # Update the number of reservations for the event
+    UpdateEventReservations(db, reservation.eid, get_event_by_id(db, reservation.eid).reservations + 1)
+    return "Reservation for user " + str(db_reservation.uid) + " at event " + str(db_reservation.eid) + " created successfully"
+
+def get_reservations_by_user(db: Session, user_id: int):
+    reservations = db.query(Reservations).filter(Reservations.uid == user_id).all()
+    if reservations is None:
+        raise HTTPException(status_code=404, detail="Reservations not found")
+    return reservations
+
+@app.get("/reservationsByUser/{user_id}")
+def read_reservations_by_user(user_id: str, db: Session = Depends(get_db)):
+    reservations = get_reservations_by_user(db, int(user_id))
+    return reservations
+
+def get_reservations_by_event(db: Session, event_id: int):
+    reservations = db.query(Reservations).filter(Reservations.eid == event_id).all()
+    if reservations is None:
+        raise HTTPException(status_code=404, detail="Reservations not found")
+    return reservations
+
+@app.get("/reservationsByEvent/{event_id}")
+def read_reservations_by_event(event_id: str, db: Session = Depends(get_db)):
+    reservations = get_reservations_by_event(db, int(event_id))
+    return reservations
